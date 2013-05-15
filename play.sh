92b261fa3486e341f2bbec499662a726e6376913
Constantin <constantin@exxxtremesys.lu>
2013-05-15 20:37:38 +0200

    play.sh now picks 10 random words out of the dictionary instead of playing all words in order

--- play.sh
+++ play.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-cat words.txt | ./game
+# you should probably download a txt dictionary of your favourite language and use that as a words.txt
+
+WORDSTOPLAY=10
+
+sort -R words.txt | fgrep -v \" | head -n $WORDSTOPLAY | ./game
 
